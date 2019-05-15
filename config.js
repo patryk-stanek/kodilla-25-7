f63c13b7dc9be40c8701df1e52ddc9908925f76d
Patryk Stanek <p.stanek3@gmail.com>
2019-05-15 17:20:32 +0200

    google api included

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '332950160452-b33gfjjkaqn0ueg6gu7l7j0vkbdekdg0.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'TT13byl2hMgT32nJO-Me4rRA',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
--- server.js
+++ server.js
@@ -1,11 +1,59 @@
 import express from 'express';
+
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const config = require('./config');
+const googleProfile = {};
+
 const app = express();
 const path = require('path');
 
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+    done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+        clientID: config.GOOGLE_CLIENT_ID,
+        clientSecret: config.GOOGLE_CLIENT_SECRET,
+        callbackURL: config.CALLBACK_URL
+    },
+    (accessToken, refreshToken, profile, cb) => {
+        googleProfile = {
+            id: profile.id,
+            displayName: profile.displayName
+        };
+        cb(null, profile);
+    }
+));
+
 app.use(express.static(path.join(__dirname, './public')));
+
 app.set('view engine', 'pug');
 app.set('views', './views');
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use((req, res, next) => {
-    next();
+app.get('/', (req, res) => {
+    res.render('index', {user: req.user});
 });
+
+app.get('/logged', (req, res) => {
+    res.render('logged', {user: googleProfile});
+});
+
+app.get('/auth/google',
+passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback',
+passport.authenticate('google', {
+    successRedirect: '/logged',
+    failureRedirect: '/'
+}));
+
+app.listen(3000);
